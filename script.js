9d499df540b4d08210987ba9eacbc902f2c104b4
exams4u <meshramkapil715@gmail.com>
2025-05-14 23:24:00 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-
-window.addEventListener('load', function() {
-  const audio = document.getElementById('background-sound');
-  audio.volume = 1; // Set volume to 50%
-  audio.play()
-    .catch(function(error) {
-      console.log("Audio playback failed:", error);
-    });
-});
